1c318993507dd4f68fd312d522de78932242aa62
Devesh Wadibhasme <deveshwadibhasme.12@gmail.com>
2025-11-08 08:29:55 +0530

    minor structure and logo font change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,11 @@
-import { useEffect } from "react";
+
 import { Outlet } from "react-router-dom";
-import AnimatedLogo from "./components/AnimatedLogo";
-import Button from "./components/Button";
-import { User2Icon } from "lucide-react";
+import WelcomePage from "./pages/welcomePage";
 
 function App() {
   return (
     <>
-      <AnimatedLogo />
-      <div>
-        <Button toLink={'/signup'} isLink={true} type="button" className="translate-y-23">
-          Sign Up <User2Icon size={20} className="ml-2" />
-        </Button>
-      </div>
-      <h1 className="text-xl font-subtitle absolute bottom-1/3">
-        Site Under Development......
-      </h1>
+      <WelcomePage />
       <Outlet />
     </>
   );
---
+++ frontend/src/pages/welcomePage.jsx
@@ -0,0 +1,34 @@
+import AnimatedLogo from "../components/AnimatedLogo";
+import Button from "../components/Button";
+import { User2, User2Icon, UserCheck2Icon, UserCheckIcon } from "lucide-react";
+
+const WelcomePage = () => {
+  return (
+    <>
+      <AnimatedLogo />
+      <h1 className="text-xl font-subtitle absolute bottom-15">
+        Site Under Development......
+      </h1>
+      <div className="flex gap-10">
+        <Button
+          toLink={"/signup"}
+          isLink={true}
+          type="button"
+          className="translate-y-23"
+        >
+          Sign Up <UserCheck2Icon size={20} className="ml-2" />
+        </Button>
+        <Button
+          toLink={"/login"}
+          isLink={true}
+          type="button"
+          className="translate-y-23"
+        >
+          Log In <UserCheckIcon size={20} className="ml-2" />
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default WelcomePage;
